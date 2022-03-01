9618625b4a287c21660f8963d1fa02bb08ecb6c3
Hazeha <krantja@gmail.com>
2022-03-02 00:22:13 +0100

    init

---
+++ controlpanel.component.ts
@@ -0,0 +1,71 @@
+
+  availableDevices: MediaDeviceInfo[];
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResultString: any;
+  deviceSelected: string;
+  deviceCurrent: MediaDeviceInfo;
+  formatsEnabled: any;
+  torchAvailable$: any;
+  torchEnabled: boolean;
+  tryHarder: boolean;
+  /** Constructor */
+  constructor(
+    private readonly _dialog: MatDialog,
+  ) {
+  }
+  /** Methods */
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const selectedStr = selected || '';
+    if (this.deviceSelected === selectedStr) { return; }
+    this.deviceSelected = selectedStr;
+    const device = this.availableDevices.find(x => x.deviceId === selected);
+    this.deviceCurrent = device || undefined;
+  }
+
+  onDeviceChange(device: MediaDeviceInfo) {
+    const selectedStr = device?.deviceId || '';
+    if (this.deviceSelected === selectedStr) { return; }
+    this.deviceSelected = selectedStr;
+    this.deviceCurrent = device || undefined;
+  }
+
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
+
+  openInfoDialog() {
+    const data = {
+      hasDevices: this.hasDevices,
+      hasPermission: this.hasPermission,
+    };
+    this.deviceCurrent = this.availableDevices[0]
+   // this._dialog.open(AppInfoDialogComponent, { data });
+  }
+
+  onTorchCompatible(isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false);
+  }
+
+  toggleTorch(): void {
+    this.torchEnabled = !this.torchEnabled;
+  }
+
+  toggleTryHarder(): void {
+    this.tryHarder = !this.tryHarder;
+  }
+}
